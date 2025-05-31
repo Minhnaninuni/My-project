f7065b2a7edc81d9313e04d2ecb5c2a452904870
Minhnaninuni <111120913+Minhnaninuni@users.noreply.github.com>
2025-05-31 15:56:39 +0700

    31/5

--- Assets/My Game/Script/Cards.cs
+++ Assets/My Game/Script/Cards.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Cards : MonoBehaviour
@@ -6,28 +8,32 @@ public class Cards : MonoBehaviour
     public Sprite backSprite;
     private SpriteRenderer sr;
     private bool isFlipped = false;
+    
 
     void Start()
     {
         sr = GetComponent<SpriteRenderer>();
-        sr.sprite = backSprite;   
+        sr.sprite = backSprite;
     }
-
     private void OnMouseDown()
     {
         FlipCard();
     }
-    public void FlipCard()
+    public void FlipCard() 
     {
-        if(isFlipped)
+        if (isFlipped)
         {
             sr.sprite = backSprite;
             isFlipped = false;
+            
         }
         else
         {
             sr.sprite = frontSprite;
             isFlipped = true;
+            
         }
     }
+
+
 }
--- Assets/My Game/Script/board.cs
+++ Assets/My Game/Script/board.cs
@@ -1,44 +1,76 @@
+
+using System.Collections.Generic;
 using UnityEngine;
 
-public class board : MonoBehaviour
+public class Board : MonoBehaviour
 {
     [Header("Card Setup")]
     public GameObject card;
     public Transform cardParent;
     public Sprite cardBackSprite;
+    public List<Sprite> listCard;
 
     [Header("Grid Setup")]
     public int rows = 4;
     public int cols = 4;
     public float spacingX = 2f;
-    public float spacingY = 3f;
+    public float spacingY = 2f;
     public Vector2 pos = new Vector2(-6f, 3f);
-    
-    {
-        
-    }
+
+
+    // Start is called before the first frame update
     void Start()
     {
-        CreateGrid()
+        CreateGrid();
     }
 
+    // Update is called once per frame
     void CreateGrid()
-{
-    for (int i = 0; i < rows; i++)
     {
-        for (int j = 0; j < cols; j++)
+        List<Sprite> ListRandom = new List<Sprite>();
+        int pairCount = (rows * cols) / 2;
+
+        for(int i  = 0; i < pairCount; i++)
         {
-            Vector2 position = new Vector2(
-                position.x + j * spacingX,
-                position.y + i * spacingY
-                );
+            Sprite Symbol = listCard[i];
+            ListRandom.Add(Symbol);
+            ListRandom.Add(Symbol);
         }
-        GameObject newCard = Instantiate(newCard, position, Quaternion.indentity, cardParent);
-        SpriteRenderer sr = newCard.GetComponent<SpriteRenderer>();
-        if (sr != null)
+        ShuffleList(ListRandom);
+        int index = 0;
+
+        for (int i = 0; i < rows; i++)
         {
-            sr.sprite = cardBackSprite;
+            for (int j = 0; j < cols; j++)
+            {
+                Vector2 position = new Vector2(
+                    pos.x + j * spacingX,// -4 // -2 // 0
+                    pos.y + i * spacingY// 3 //5
+                    );
+                GameObject newCard = Instantiate(card, position, Quaternion.identity, cardParent);
+                Cards sr = newCard.GetComponent<Cards>();
+                if (sr != null)
+                {
+                    sr.backSprite = cardBackSprite;
+                    sr.frontSprite = ListRandom[index];
+                }
+                index++;
+            }
+        }
+
+        
+    }
+
+    void ShuffleList<T>(List<T> list)
+    {
+        for (int i = 0; i<list.Count; i++)
+        {
+            int rand = Random.Range(i, list.Count);
+            T Temp = list[i];
+            list[i] = list[rand];
+            list[rand] = Temp;
         }
     }
-    }
+
+
 }
